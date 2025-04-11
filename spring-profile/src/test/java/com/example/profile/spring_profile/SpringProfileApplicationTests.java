bb201f9f221de89d58dbb4815e86356c8fc69b1c
hayan <hayan.tariq@gmail.com>
2025-04-11 18:29:22 -0500

    Spring Profile initial commit

---
+++ spring-profile/src/test/java/com/example/profile/spring_profile/SpringProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.profile.spring_profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
